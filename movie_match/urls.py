5e479067843c805b84b676d9595feb4cbb6fb513
tugrulguner <tugrulgunr@gmail.com>
2023-07-10 09:09:27 -0400

    new url added for tmdb

--- movie_match/urls.py
+++ movie_match/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
-from movie_match.api.endpoints import MovieMatch
+from movie_match.api.endpoints import MovieMatch, TMDBResult
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 
 router.register(r'movie-match', MovieMatch, basename='movie-match')
+router.register(r'tmdb-result', TMDBResult, basename='tmdb-result')
+# router.register(r'tmdb-result', MovieMatch.as_view({'post': 'tmdb_return'}), basename='tmdb-result')
 
 urlpatterns = [
     path('', include(router.urls)),
