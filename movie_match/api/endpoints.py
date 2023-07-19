99be43ff2a65df637c70942657f0bddbed9812b7
ozge <ozgecaylioglu@gmail.com>
2023-07-18 21:36:29 -0400

    added search endpoint

--- movie_match/api/endpoints.py
+++ movie_match/api/endpoints.py
@@ -11,7 +11,7 @@ from sentence_transformers import (
 )
 
 load_dotenv()
-
+TMDB_TOKEN = os.getenv("TMDB_TOKEN")
 class MovieMatch(viewsets.GenericViewSet):
     
     def __init__(self, *args, **kwargs):
@@ -35,31 +35,3 @@ class MovieMatch(viewsets.GenericViewSet):
         }
         return JsonResponse(filtered_result)
    
-class TMDBResult(viewsets.GenericViewSet) :
-    
-    def __init__(self, *args, **kwargs):
-        pass
-
-    def create(self, request) -> JsonResponse:
-        ai_return = requests.post(
-            'http://127.0.0.1:8000/movie-match/', 
-            json={
-                'user_input': request.data['user_input'],
-                'top_k': request.data['top_k']
-            }
-        )
-        tmdb_return_dict = {}
-        for _, movie_name in ai_return.json().items():
-            tmdb_return = requests.get(
-                f'https://api.themoviedb.org/3/search/movie?query={movie_name}',
-                headers = {
-                    'accept': 'application/json',
-                    'Authorization': f'Bearer {os.getenv("tmdb_token")}'
-                }
-            )
-            tmdb_return_dict[movie_name] = [
-                element for element in tmdb_return.json()['results'] 
-                    if movie_name.lower()==element['original_title'].lower() 
-            ]
-            
-        return JsonResponse(tmdb_return_dict)
--- movie_match/urls.py
+++ movie_match/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path, include
-from movie_match.api.endpoints import MovieMatch, TMDBResult
+from movie_match.api.endpoints import MovieMatch
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 
-router.register(r'movie-match', MovieMatch, basename='movie-match')
-router.register(r'tmdb-result', TMDBResult, basename='tmdb-result')
-# router.register(r'tmdb-result', MovieMatch.as_view({'post': 'tmdb_return'}), basename='tmdb-result')
+router.register(r'movie/suggestion', MovieMatch, basename='movie-match')
 
 urlpatterns = [
     path('', include(router.urls)),
